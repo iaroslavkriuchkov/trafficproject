be338220aec466d0098549cff56f7aeef437fb68
Iaroslav Kriuchkov <iaroslav.kriuchkov@gmail.com>
2021-10-13 14:50:49 +0300

    Basic aggregation for speed an flow introduced; The scatter plot in speed-flow plane introduced

--- try.py
+++ try.py
@@ -4,10 +4,11 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Function for downloading csv data from an automatic traffic monitoring system of Finnish Transport Agency
 # url is the link to the exact file
-# function returns the Pandas DataFrame with the data from specific TMS point on specific year on specific day
+# The function returns the Pandas DataFrame with the data from specific TMS point on specific year on specific day
 def data_import (tms_id, region, year, day):
     column_names = ['id', 'year', 'day', 'hour', 'minute', 'second', '1/100 second', 'length', 'lane', 'direction', 'vehicle', 'speed', 'faulty', 'Total time', 'Time interval', 'Queue start' ]
     df = pd.DataFrame()
@@ -16,6 +17,32 @@ def data_import (tms_id, region, year, day):
     df = pd.read_csv(url, delimiter = ";", names = column_names)
     return df
 
+# The DataFrame is created based on downloaded data
 df = pd.DataFrame() 
-df = data_import('101', '01', 2017, 1)
-print(df.head())
+df = data_import('117', '01', 2017, 100)
+#print(df.head())
+
+# aggregation_time_period shows the period, by which the data is aggregated (in minutes)  
+aggregation_time_period = 60
+
+# flow_list is the list, which calculates the flow with each of aggregation periods
+# speed_list is the list, which calculates the total speed of the vehicles with each of aggregation periods
+flow_list = [0 for i in range(int(24*60/aggregation_time_period))]
+speed_list = [0 for i in range(int(24*60/aggregation_time_period))]
+
+# Calcuation of flow and total speed
+for i in range(len(df)):    
+    flow_list[int(df["Total time"][i]/(aggregation_time_period*60*100))] += 1
+    speed_list[int(df["Total time"][i]/(aggregation_time_period*60*100))] += df["speed"][i]
+
+# Calcuation of arifmetic mean speed within the aggregation period
+mean_speed_list = [i / j for i, j in zip(speed_list, flow_list)]
+print(mean_speed_list)
+
+# The scatter plot in speed-flow plane
+plt.scatter(flow_list, mean_speed_list)
+plt.show()
+
+
+
+
