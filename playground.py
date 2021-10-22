a66630eaf2ad99f4f0cbe03e96d7ef1bfed438e8
Iaroslav Kriuchkov <iaroslav.kriuchkov@gmail.com>
2021-10-22 19:45:09 +0300

    Changes Ootober 22

---
+++ playground.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+name = input("Input name: \n")
+
+no_space=name.replace(" ","")
+print(no_space)
--- traffic.py
+++ traffic.py
@@ -5,21 +5,30 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import requests
 
 # Function for downloading csv data from an automatic traffic monitoring system of Finnish Transport Agency
 # url is the link to the exact file
 # The function returns the Pandas DataFrame with the data from specific TMS point on specific year on specific day
-def data_import (tms_id, region, year, day):
+def file_import (tms_id, region, year, day):
+    start_time = time.perf_counter() 
     column_names = ['id', 'year', 'day', 'hour', 'minute', 'second', 'hund_second', 'length', 'lane', 'direction', 'vehicle', 'speed', 'faulty', 'total_time', 'time_interval', 'queue_start' ]
     df = pd.DataFrame()
     url = 'https://aineistot.liikennevirasto.fi/lam/rawdata/YYYY/REGION_ID/lamraw_TMS_YY_DD.csv'
     url = url.replace('YYYY', str(year)).replace('REGION_ID', region).replace('TMS', tms_id).replace('YY', str(year)[2:4]).replace('DD', str(day))
-    df = pd.read_csv(url, delimiter = ";", names = column_names)
+    if requests.get(url).status_code != 404:
+        df = pd.read_csv(url, delimiter = ";", names = column_names)
+        print(f"Download successful - file for the sensor {tms_id} for the day {day} in year {year}")
+    else:
+        print(f"File for the sensor {tms_id} for the day {day} in year {year} doesn't exist. ")
+    end_time = time.perf_counter()
+    print(f"Download took {end_time-start_time:0.4f} seconds")
     return df
 
 # The DataFrame is created based on downloaded data
 df = pd.DataFrame() 
-df = data_import('117', '01', 2017, 100)
+df = file_import('117', '01', 2017, 100)
 print(df.head())
 
 '''
