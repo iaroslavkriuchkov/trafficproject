8fbaf32ae28d559bbd39710cd273d073d2942a53
Iaroslav Kriuchkov <iaroslav.kriuchkov@gmail.com>
2021-11-03 16:12:58 +0200

    Calculation of speed and flow is now a function with 2 nodes: arithmetic and harmonic

--- traffic.py
+++ traffic.py
@@ -17,13 +17,16 @@ downdload_list = [['117', '01', 2017, 100],['117', '01', 2017, 101], ['117', '01
 # The function returns the Pandas DataFrame with the data from specific TMS point on specific year on specific day
 def file_import (tms_id, region, year, day):
     start_time = time.perf_counter() 
-    column_names = ['id', 'year', 'day', 'hour', 'minute', 'second', 'hund_second', 'length', 'lane', 'direction', 'vehicle', 'speed', 'faulty', 'total_time', 'time_interval', 'queue_start' ]
+    column_names = ['id', 'year', 'day', 'hour', 'minute', 'second', 'hund_second', 'length', 'lane', 'direction', 'vehicle', 'speed', 'faulty', 'total_time', 'time_interval', 'queue_start']
     df = pd.DataFrame()
     url = 'https://aineistot.liikennevirasto.fi/lam/rawdata/YYYY/REGION_ID/lamraw_TMS_YY_DD.csv'
     url = url.replace('YYYY', str(year)).replace('REGION_ID', region).replace('TMS', tms_id).replace('YY', str(year)[2:4]).replace('DD', str(day))
     if requests.get(url).status_code != 404:
         df = pd.read_csv(url, delimiter = ";", names = column_names)
         print(f"Download successful - file for the sensor {tms_id} for the day {day} in year {year}")
+        for i in range(len(df)):
+            if df.at[i, "faulty"]== 1:
+                df.drop(i, axis=0, inplace=True)
     else:
         print(f"File for the sensor {tms_id} for the day {day} in year {year} doesn't exist. ")
     end_time = time.perf_counter()
@@ -42,44 +45,42 @@ def data_import(input_list):
             df = df.append(df1, ignore_index=True)
     return df
 
+# Calculation of flow and speed lists. Mode 0 is the arithmetic mean of speed, mode 1 is the harmonic mean of speed
+def flow_speed_calculation (df, aggregation_time_period, mode):
+    start_time = time.perf_counter() 
+    flow_speed = pd.DataFrame()
+    flow_speed['flow'] = [0 for i in range(int(24*60/aggregation_time_period))]
+    flow_speed['total_speed'] = [0.0 for i in range(int(24*60/aggregation_time_period))]
+    flow_speed['mean_speed'] = [0.0 for i in range(int(24*60/aggregation_time_period))]
+#   '''
+    if mode == 0:
+        for i in range(len(df)):      
+            flow_speed.at[int(df["total_time"][i]/(aggregation_time_period*60*100)), 'flow'] += 1
+            flow_speed.at[int(df["total_time"][i]/(aggregation_time_period*60*100)), 'total_speed'] += df.at[i, 'speed']
+        flow_speed['mean_speed'] = flow_speed['total_speed'].div(flow_speed['flow'].values)
+    elif mode == 1:
+        for i in range(len(df)):    
+            flow_speed.at[int(df["total_time"][i]/(aggregation_time_period*60*100)), 'flow'] += 1
+            flow_speed.at[int(df["total_time"][i]/(aggregation_time_period*60*100)), 'total_speed'] += (1 / df.at[i, "speed"])
+        flow_speed['mean_speed'] = flow_speed['flow'].div(flow_speed['total_speed'].values)
+    end_time = time.perf_counter()
+    print(f"Execution time {end_time-start_time:0.4f} seconds")
+
+    return flow_speed
 
 # The DataFrame is created based on downloaded data
 df = pd.DataFrame() 
 df = data_import(downdload_list)
 print(df.head())
 print(len(df))
-#print(df['total_time'][1015])
 
-#'''
 # aggregation_time_period shows the period, by which the data is aggregated (in minutes)  
 aggregation_time_period = 10
 
-# flow_list is the list, which calculates the flow with each of aggregation periods
-# speed_list is the list, which calculates the total speed of the vehicles with each of aggregation periods
-flow_list = [0 for i in range(int(24*60/aggregation_time_period))]
-speed_list = [0 for i in range(int(24*60/aggregation_time_period))]
-'''
-# Harmonic mean speed
-for i in range(len(df)):    
-    flow_list[int(df["total_time"][i]/(aggregation_time_period*60*100))] += 1
-    speed_list[int(df["total_time"][i]/(aggregation_time_period*60*100))] += (1/df["speed"][i])
-# Calcuation of arifmetic mean speed within the aggregation period
-mean_speed_list = [1 / (i / j) for i, j in zip(speed_list, flow_list)]
-
-
-
-'''
-# Arifmetic mean speed
-# Calcuation of flow and total speed
-for i in range(len(df)):    
-    flow_list[int(df["total_time"][i]/(aggregation_time_period*60*100))] += 1
-    speed_list[int(df["total_time"][i]/(aggregation_time_period*60*100))] += df["speed"][i]
-
-# Calcuation of arifmetic mean speed within the aggregation period
-mean_speed_list = [i / j for i, j in zip(speed_list, flow_list)]
-print(mean_speed_list)
+flow_speed = flow_speed_calculation(df, aggregation_time_period, 1)
+print(flow_speed.describe())
 
 # The scatter plot in speed-flow plane
-plt.scatter(flow_list, mean_speed_list)
+plt.scatter(flow_speed.flow, flow_speed.mean_speed)
 plt.show()
-#'''
+
